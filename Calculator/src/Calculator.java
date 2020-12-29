86eb5044cdbb7b6e14df0349aac673fa50d26e0d
hp <hp@Sandesh>
2020-12-29 16:49:08 +0530

    My first program

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,1 +1,20 @@
 
+
+public class Calculator
+{
+	public static void main(String[] args) 
+	{
+   
+	int n=10,t1=0,t2=1;
+	System.out.print("First " + n + " terms: ");
+	for(int i=1;i<=n;i++)                   //It will Check from 1 to 10 , loop will 
+		                                    //work for ten times.
+	{
+		System.out.print(t1 + " + ");
+        int sum=t1+t2;
+		t1=t2;
+		t2=sum;
+	}
+	System.out.println();
+	}
+	}
